b2c35d00c40ba089be59bc646295bee6ecdfa5d7
Alisha McGraw <alisha.b.mcgraw@gmail.com>
2025-10-14 13:25:10 -0600

    first commit

---
+++ Hangman_Project.py
@@ -0,0 +1,18 @@
+
+#Dictionary of word choices for the user
+WORD_CATEGORIES = {
+    'ANIMAL': [],
+    'FRUIT': ['PINEAPPLE', 'STRAWBERRY', 'GRAPES', 'APPLE', 'BLUEBERRY'],
+    'SPORT': ['BASKETBALL', 'FOOTBALL', 'BASEBALL', 'VOLLEYBALL', 'SOCCER']
+}
+
+# Maximum number of incorrect guesses allowed
+MAX_ATTEMPTS = 6
+
+# Ask the user to choose a category
+
+
+# Take the choosen category and pick a word for the game
+
+
+# Ask the user to pick a letter
